1ee16d36dbcf1c9a2f469ad0b3be2eaa77c67249
Adrianfeloh1 <adrianfeloh1@gmail.com>
2023-10-15 10:00:18 -0500

    Form responsive y nuevos estlos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,9 @@ export const About = () => {
 
         <section className=" md:flex md:justify-center md:items-center md:h-[760px] bg-[#171717] ">
 
-            <article className=" h-[535px] sm:h-[400px] md:flex flex-col justify-center bg-[#f55138e0]
+            <article className=" h-[520px] sm:h-[400px] md:flex flex-col justify-center bg-[#f55138e0]
                                 md:ml-8 md:h-[650px] md:w-[850px] ">
-                <div className=" p-10 sm:p-16 md:mr-24 ">
+                <div className=" p-8 sm:p-16 md:mr-24 ">
                     <h2 className=" text-2xl mb-8">ABOUT</h2>
                     <p>As a web developer and user experience (UX) designer,
                         I find myself at the intersection of technology and design,
@@ -26,7 +26,7 @@ export const About = () => {
 
             <div>
                 <Fade fraction={0.8} triggerOnce={true} >
-                    <img className=" m-auto relative bottom-20 max-h-[320px] sm:max-h-[500px] sm:relative
+                    <img className=" m-auto relative bottom-20 max-h-[370px] sm:max-h-[500px] sm:relative
                         sm:bottom-16 md:relative md:bottom-0 md:right-32 md:max-h-[550px]" src={profile} alt="Image profile" />
                 </Fade>
             </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,33 +4,40 @@ import { GrTwitter } from "react-icons/gr";
 
 export const Form = () => {
 
-    return (
-        <section className=" bg-[#171717] h-[550px] flex justify-center items-center px-10 pb-20">
+    const currentYear = new Date().getFullYear();
 
-            <article className=" flex flex-col w-1/2 text-center px-20">
-                <h2 className="text-4xl mb-10 uppercase text-[white]">Let's Discuss Your Project!</h2>
-                <p className="mb-10 text-[#c2c2c2]"> I'm thrilled about the opportunity to collaborate on your project. Please,
-                    complete the following form with your contact details. I will get back to you
-                    as soon as possible to discuss how I can assist you in developing your website.
+    return (
+        <section className=" bg-[#171717] flex flex-col md:flex md:flex-row justify-center items-center px-10 pb-20">
+
+            <article className=" flex flex-col w-2/2 md:w-1/2 text-center md:px-8 lg:px-20">
+                <h2 className=" md:text-2xl lg:text-4xl mb-5 lg:mb-10 uppercase text-[white]">
+                    Let's Discuss Your Project!
+                </h2>
+                <p className="mb-10 text-[#c2c2c2]"> I'm thrilled about the opportunity to collaborate
+                    on your project. Please, complete the following form with your contact details. I will get
+                    back to you as soon as possible to discuss how I can assist you in developing your website.
                 </p>
-                <h3 className="mb-2">E-mail</h3>
+                <h3 className="mb-2 md:text-lg lg:text-2xl">E-mail</h3>
                 <p className=" text-[#c2c2c2]">adrianfeloh1@gmail.com</p>
-                <div className="mt-4 text-2xl flex justify-center items-center gap-4 text-[white]">
+                <div className="mt-4 mb-4 md:mb-16 text-2xl flex justify-center items-center gap-4 text-[white]">
                     <GrLinkedin />
                     <GrInstagram />
                     <GrTwitter />
                 </div>
+
+                <p className="pb-5">© AF {currentYear} - Todos los derechos reservados.</p>
+
             </article>
 
-            <form action="" className=" bg-[#171717] flex flex-col w-1/2 px-20 border p-10 text-[white] ">
-                <h2 className=" text-4xl text-center mb-8 text-[#f55138e0]">CONTACT FORM</h2>
+            <form action="" className=" bg-[#171717] flex flex-col w-full md:w-1/2 px-10 lg:px-20 border-8 p-10 text-[white] ">
+                <h2 className=" md:text-2xl lg:text-4xl text-center mb-8 text-[#f55138e0]">CONTACT FORM</h2>
 
                 <label htmlFor="name" className="mb-2">Your Name</label>
-                <input type="text" id="name" name="Your Name" required
+                <input type="text" id="name" name="Your Name" autoComplete="name" required
                     className="outline-none bg-[#171717] border-b mb-6" />
 
                 <label htmlFor="email" className="mb-2">Email:</label>
-                <input type="email" id="email" name="email" required
+                <input type="email" id="email" name="email" autoComplete="email" required
                     className="outline-none bg-[#171717] border-b mb-6" />
 
                 <label htmlFor="mesage" className="mb-2">How can I assist you?</label>
@@ -39,14 +46,13 @@ export const Form = () => {
 
                 <div className=" flex justify-center">
                     <button type="submit"
-                        className="bg-[#f55138e0] w-1/2 font-bold uppercase text-white border-none px-8 py-3 ">
-                        Send Message
+                        className="bg-[#f55138e0] w-1/2 font-bold uppercase text-white border-none py-3 ">
+                        Send <span className="hidden lg:inline"> Message</span>
                     </button>
-
-                </div>
+                </div>                
 
             </form>
-
+            
         </section>
     )
 }
