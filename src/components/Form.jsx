fc03bcfdbeefd70c158685997e278f7500e20337
Adrianfeloh1 <adrianfeloh1@gmail.com>
2024-06-02 11:23:01 -0500

    Agregando spinner a la espera del envio del formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,8 +9,10 @@ export const Form = () => {
     const { t } = useTranslation();
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const [successMessage, setSuccessMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const send = async (data) => {
+        setIsLoading(true); // Comienza el loading
         try {
             await addDoc(collection(db, "contacts"), {
                 name: data.name,
@@ -28,6 +30,8 @@ export const Form = () => {
         } catch (e) {
             console.error("Error al enviar el mensaje: ", e);
             setSuccessMessage(t("Hubo un error al enviar el mensaje"));
+        } finally {
+            setIsLoading(false); // Finaliza el loading
         }
     };
 
@@ -115,8 +119,32 @@ export const Form = () => {
                     <button
                         type="submit"
                         className="bg-[#f55138e0] dark:bg-[#134A66] w-1/2 font-bold uppercase text-white border-none py-3"
+                        disabled={isLoading}
                     >
-                        {t("send")}
+                        {isLoading ? (
+                            <svg
+                                className="animate-spin h-5 w-5 mr-3 text-white inline"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    className="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    strokeWidth="4"
+                                ></circle>
+                                <path
+                                    className="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                                ></path>
+                            </svg>
+                        ) : (
+                            t("send")
+                        )}
                     </button>
                 </div>
             </form>
